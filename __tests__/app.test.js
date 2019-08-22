3c4dd89d67c2c2a5cbd8ec2b00fdc567554362a5
joannaarroyo <joliuac@gmail.com>
2019-08-21 17:38:31 -0700

    wrote test for get/post/put/delete for products in app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -73,3 +73,77 @@ describe('Categories API', () => {
       })
   });
 });
+
+describe('Products API', () => {
+  test('Can post a new product, returns saved product', () => {
+    const testProduct = {
+      name: 'smelly smells',
+      ranking: -50,
+      invisible: true
+    };
+
+    return mockRequest.post('/api/v1/products')
+      .send(testProduct)
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.name).toEqual('smelly smells');
+      });
+  });
+
+  test('Can get existing products', () => {
+    return mockRequest.get('/api/v1/products')
+      .then(response => {
+        expect(response.status).toEqual(200);
+        expect(response.body.results.count).toEqual(1);
+        expect(response.body.results.results[0].name).toEqual('smelly smells');
+      });
+  });
+
+  test('Can modify existing product', () => {
+    const testProductTwo = {
+      name: 'baby birds',
+      ranking: 123,
+      invisible: false
+    };
+
+    const updatedProduct = {
+      name: 'puppies',
+      ranking: 156,
+      invisible: false
+    };
+
+    return mockRequest.post('/api/v1/products')
+      .send(testProductTwo)
+      .then(response => {
+        return mockRequest.put(`/api/v1/products/${response.body._id}`)
+          .send(updatedProduct)
+          .then(response => {
+            expect(response.status).toEqual(200);
+            expect(response.body.name).toEqual('puppies');
+          })
+      })
+  });
+
+  test('Can delete existing product', () => {
+    const testProductThree = {
+      name: 'hiccups',
+      ranking: 2,
+      invisible: true
+    };
+
+    return mockRequest.post('/api/v1/products')
+      .send(testProductThree)
+      .then(response => {
+        return mockRequest.delete(`/api/v1/products/${response.body._id}`)
+          .then(response => {
+            expect(response.status).toEqual(200);
+            expect(response.body.name).toEqual('hiccups');
+
+            return mockRequest.get('/api/v1/products')
+              .then(response => {
+                expect(response.body.results.count).toEqual(2);
+              })
+          });
+      })
+  });
+});
